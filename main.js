3afa8f1e90c0a613c34d2eb1c3bb4cff3dfeb97f
Jasonltc <jasonltc_14@hotmail.com>
2023-07-24 17:17:40 +0400

    add feature

--- main.js
+++ main.js
@@ -1,39 +1,59 @@
-// career path section
-// scramble.js
-const phrases = [
-  "Hola,",
-  "Welcome Abort",
-  "Enjoy your flight",
-  "Have a safe flight and seat your belt",
-  "Ready",
-  "Go",
-  "enjoying :)",
-];
-const el = document.querySelector(".flight-text");
-const fx = new TextScramble(el);
-let counter = 0;
-
-const next = () => {
-  fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 800);
+//mobile menu active
+const menuMobileActive = function () {
+  const menuButton = document.querySelector(".menu");
+  const menu = document.querySelector(".main_header--section-items");
+  menuButton.addEventListener("click", function () {
+    menu.classList.toggle("active");
   });
-  counter = (counter + 1) % phrases.length;
 };
-next();
 
-// add up button
-$(window).on("scroll", function () {
-  if ($(this).scrollTop() > 300) {
-    $(".to-the-top").fadeIn(600);
-  } else {
-    $(".to-the-top").fadeOut(600);
-  }
-});
+// scramble.js
+function scramble() {
+  const phrases = [
+    "Hola,",
+    "Welcome Abort",
+    "Enjoy your flight",
+    "Have a safe flight and seat your belt",
+    "Ready",
+    "Go",
+    "enjoying :)",
+  ];
+  const el = document.querySelector(".flight-text");
+  const fx = new TextScramble(el);
+  let counter = 0;
 
-$(".top").on("click", function (e) {
-  e.preventDefault();
-  $("html,body").animate({ scrollTop: 0 }, 300);
-});
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 800);
+    });
+    counter = (counter + 1) % phrases.length;
+  };
+  next();
+}
+
+//accordion for skill
+const accordion = document.querySelectorAll(".skill-item");
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
+
+function addButtonUp() {
+  // add up button
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 300) {
+      $(".to-the-top").fadeIn(600);
+    } else {
+      $(".to-the-top").fadeOut(600);
+    }
+  });
+
+  $(".top").on("click", function (e) {
+    e.preventDefault();
+    $("html,body").animate({ scrollTop: 0 }, 300);
+  });
+}
 
 //swiper
 var swiper = new Swiper(".mySwiper", {
@@ -54,3 +74,7 @@ var swiper = new Swiper(".mySwiper", {
     slideShadows: true,
   },
 });
+
+menuMobileActive();
+addButtonUp();
+scramble();
