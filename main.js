587307149961b7873c43fd446cd93fe0ef8a5df5
Jasonltc <jasonltc_14@hotmail.com>
2023-12-18 22:17:50 +0400

    header modification

--- main.js
+++ main.js
@@ -1,92 +1,94 @@
-//mobile menu active
-const menuMobileActive = function () {
-  const menuButton = document.querySelector(".menu");
-  const menu = document.querySelector(".menu-item");
-  menuButton.addEventListener("click", function () {
-    menu.classList.toggle("active");
-  });
-};
-
-// scramble.js
-function scramble() {
-  const phrases = [
-    "Welcome Abort",
-    "Enjoy your flight",
-    "Have a safe flight and seat your belt",
-    "Ready",
-    "Go",
-    "enjoying :)",
-  ];
-  const el = document.querySelector(".flight-text");
-  const fx = new TextScramble(el);
-  let counter = 0;
-
-  const next = () => {
-    fx.setText(phrases[counter]).then(() => {
-      setTimeout(next, 800);
+$("document").ready(function () {
+  //mobile menu active
+  const menuMobileActive = function () {
+    const menuButton = document.querySelector(".menu");
+    const menu = document.querySelector(".menu-item");
+    menuButton.addEventListener("click", function () {
+      menu.classList.toggle("active");
     });
-    counter = (counter + 1) % phrases.length;
   };
-  next();
-}
 
-function addButtonUp() {
-  // add up button
-  $(window).on("scroll", function () {
-    if ($(this).scrollTop() > 300) {
-      $(".to-the-top").fadeIn(600);
-    } else {
-      $(".to-the-top").fadeOut(600);
-    }
+  // scramble.js
+  function scramble() {
+    const phrases = [
+      "Welcome Abort",
+      "Enjoy your flight",
+      "Have a safe flight and seat your belt",
+      "Ready",
+      "Go",
+      "enjoying :)",
+    ];
+    const el = document.querySelector(".flight-text");
+    const fx = new TextScramble(el);
+    let counter = 0;
+
+    const next = () => {
+      fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 800);
+      });
+      counter = (counter + 1) % phrases.length;
+    };
+    next();
+  }
+
+  function addButtonUp() {
+    // add up button
+    $(window).on("scroll", function () {
+      if ($(this).scrollTop() > 300) {
+        $(".to-the-top").fadeIn(600);
+      } else {
+        $(".to-the-top").fadeOut(600);
+      }
+    });
+
+    $(".to-the-top").on("click", function (e) {
+      e.preventDefault();
+      $("html,body").animate({ scrollTop: 0 }, 300);
+    });
+  }
+
+  //swiper
+  var swiper = new Swiper(".mySwiper", {
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+    loop: true,
   });
 
-  $(".to-the-top").on("click", function (e) {
-    e.preventDefault();
-    $("html,body").animate({ scrollTop: 0 }, 300);
-  });
-}
+  // gsap animation
+  gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
-//swiper
-var swiper = new Swiper(".mySwiper", {
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-  },
-  loop: true,
+  if (window.innerWidth >= 1024) {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".my_journey",
+        start: "top top",
+        end: "bottom 300",
+        scrub: "true",
+        pin: "true",
+      },
+    });
+
+    tl.to(".airplane", { y: 1270, color: "#ee4036" });
+  }
+
+  menuMobileActive();
+  addButtonUp();
+  scramble();
 });
-
-// gsap animation
-gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
-
-if (window.innerWidth >= 1024) {
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".my_journey",
-      start: "top top",
-      end: "bottom 300",
-      scrub: "true",
-      pin: "true",
-    },
-  });
-
-  tl.to(".airplane", { y: 1270, color: "#ee4036" });
-}
-
-menuMobileActive();
-addButtonUp();
-scramble();
